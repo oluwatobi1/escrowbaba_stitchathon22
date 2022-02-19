52eff0df6a1b98a4b12b2c80b68e9e9fa27de133
Opetunde Adepoju <helenopetunde@gmail.com>
2022-02-19 11:00:47 +0000

    setup profile route

--- src/views/routes/index.jsx
+++ src/views/routes/index.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Merchant from '../merchant/index';
+import Profile from '../profile/index';
 
 const AllRoutes = () => {
 
     return (
         <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Merchant />}>
+        <Route path="/" element={<Merchant />}/>
+        <Route path="/profile" element={<Profile />}/>
           <Route index element={<Merchant />} />
-        </Route>
       </Routes>
     </BrowserRouter>
 
