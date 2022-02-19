697f07f75b71c10f3face734b2dff15c9b6a34df
oluwatobi1 <akintunlesetobi@gmail.com>
2022-02-19 13:29:30 +0100

    pricing template

--- src/views/merchant/index.jsx
+++ src/views/merchant/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from "../utils/NavBar"
+import Pricing from './components/Pricing';
 
 const Merchant = () => {
   return (
     <>
     <NavBar/>
+    <Pricing/>
     <div> hello Merchant</div>
     </>
   )
