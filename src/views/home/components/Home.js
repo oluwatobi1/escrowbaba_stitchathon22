b351defd8b4a224ce85aafb5a1ee8e7f77a6c7bf
Opetunde Adepoju <helenopetunde@gmail.com>
2022-02-19 14:39:36 +0000

    create form for homepage

--- src/views/home/components/Home.js
+++ src/views/home/components/Home.js
@@ -13,6 +13,11 @@ import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
 import GlobalStyles from '@mui/material/GlobalStyles';
 import Container from '@mui/material/Container';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Form from "./Form"
 
 function Copyright(props) {
   return (
@@ -31,43 +36,6 @@ function Copyright(props) {
 }
 
 const tiers = [
-  {
-    title: 'Free',
-    price: '0',
-    description: [
-      '10 users included',
-      '2 GB of storage',
-      'Help center access',
-      'Email support',
-    ],
-    buttonText: 'Sign up for free',
-    buttonVariant: 'outlined',
-  },
-  {
-    title: 'Pro',
-    subheader: 'Most popular',
-    price: '15',
-    description: [
-      '20 users included',
-      '10 GB of storage',
-      'Help center access',
-      'Priority email support',
-    ],
-    buttonText: 'Get started',
-    buttonVariant: 'contained',
-  },
-  {
-    title: 'Enterprise',
-    price: '30',
-    description: [
-      '50 users included',
-      '30 GB of storage',
-      'Help center access',
-      'Phone & email support',
-    ],
-    buttonText: 'Contact us',
-    buttonVariant: 'outlined',
-  },
 ];
 
 const footers = [
@@ -202,6 +170,20 @@ function PricingContent() {
           ))}
         </Grid>
       </Container>
+<Grid container alignItems="center"
+justifyContent="center">
+  <Grid item
+  sx={{  width:"50vw"}}
+  >
+<Form/>
+
+  </Grid>
+
+</Grid>
+
+
+
+
       {/* Footer */}
       <Container
         maxWidth="md"
